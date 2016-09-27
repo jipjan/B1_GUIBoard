851f8eb75a8f71b5caf69e629eb97dee5cf2b732
Jaap-Jan de Wit <jipjan@gmail.com>
2016-09-27 10:14:38 +0200

    part 1 of assigment 1: count digit 3 times from 0 to 9

--- GUIBoard/GUI_Gebruik.java
+++ GUIBoard/GUI_Gebruik.java
@@ -10,9 +10,16 @@ public class GUI_Gebruik
         IO.init();
     }
     
+    public void countFirstDigit() {
+        for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 10; j++) {              
+            IO.writeShort(0x10, j);
+            IO.delay(100);
+        }
+    }
     
-   public static void testWerking() {
-       IO.init();
-       IO.writeShort(0x22, 7); // Display het  (decimale)  getal  7  op element 0x22
-   }
+    public static void testWerking() {
+        IO.init();
+        IO.writeShort(0x22, 7); // Display het  (decimale)  getal  7  op element 0x22
+    }
 }
