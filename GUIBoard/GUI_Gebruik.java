645e0a1d28939224676407b4d8af4c129e0f3c2c
Robin Hobbel <rm.hobbel@telfort.nl>
2016-09-27 10:25:44 +0200

    opdracht 1 part 2, count all digits from 0 to 9

--- GUIBoard/GUI_Gebruik.java
+++ GUIBoard/GUI_Gebruik.java
@@ -18,6 +18,18 @@ public class GUI_Gebruik
         }
     }
     
+    public void countAllDigits() {
+        for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 10; j++) {              
+            IO.writeShort(0x10, j);
+            IO.writeShort(0x12, j);
+            IO.writeShort(0x14, j);
+            IO.writeShort(0x16, j);
+            IO.writeShort(0x18, j);
+            IO.delay(100);
+        }
+    }
+    
     public static void testWerking() {
         IO.init();
         IO.writeShort(0x22, 7); // Display het  (decimale)  getal  7  op element 0x22
