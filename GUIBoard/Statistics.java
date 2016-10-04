3c048fd4e63e4ecd936c0f907f9ac5ec95c569d2
Robin Hobbel <rm.hobbel@telfort.nl>
2016-10-04 15:10:39 +0200

    Made Mode and lowest

--- GUIBoard/Statistics.java
+++ GUIBoard/Statistics.java
@@ -10,43 +10,207 @@ public class Statistics
     {
         double average = 0;
         for(int i = 0; i < list.size();i++)
-        switch (kindOf)
-        {
-            case InsideTemp:
-            average += list.get(i).getInsideTemp();
-            break;
+            switch (kindOf)
+            {
+                case InsideTemp:
+                average += list.get(i).getInsideTemp();
+                break;
 
-            case OutsideTemp:
-            average += list.get(i).getOutsideTemp();
-            break;
+                case OutsideTemp:
+                average += list.get(i).getOutsideTemp();
+                break;
 
-            case Windspeed:
-            average += list.get(i).getWindSpeed();
-            break;
-            
-            case OutsideHum:
-            average += list.get(i).getOutsideHum();
-            break;
-            
-            case RainRate:
-            average += list.get(i).getRainRate();
-            break;
-            
-            case UVLevel:
+                case Windspeed:
+                average += list.get(i).getWindSpeed();
+                break;
+
+                case OutsideHum:
+                average += list.get(i).getOutsideHum();
+                break;
+
+                case RainRate:
+                average += list.get(i).getRainRate();
+                break;
+
+                case UVLevel:
                 average += list.get(i).getUVLevel();
-            break;
-            
-            case Solarrad:
+                break;
+
+                case Solarrad:
                 average += list.get(i).getSolarRad();
-            break;
-            
-            case Barometer:
+                break;
+
+                case Barometer:
                 average += list.get(i).getBarometer();
-            break;
-        }
-        
+                break;
+            }
+
         return average /list.size();
+    } 
+
+    /**
+     * Assists getLowest.
+     */
+    public double lowestHelper(double L, double lowest)
+    {
+        if(lowest > L)
+        {
+            lowest = L;
+        }
+        return lowest;
+    }
+    public double getLowest(ArrayList<RawMeasurement> list, Unit kindOf)
+    {
+        double lowest = Integer.MAX_VALUE;
+        for(int i = 0; i < list.size();i++)
+            switch (kindOf)
+            {
+                case InsideTemp:
+                lowest = lowestHelper(list.get(i).getInsideTemp(), lowest);
+                break;
+
+                case OutsideTemp:
+                lowest = lowestHelper(list.get(i).getOutsideTemp(), lowest);
+                break;
+
+                case Windspeed:
+                lowest = lowestHelper(list.get(i).getWindSpeed(), lowest);
+                break;
+
+                case OutsideHum:
+                lowest = lowestHelper(list.get(i).getOutsideHum(), lowest);
+                break;
+
+                case RainRate:
+                lowest = lowestHelper(list.get(i).getRainRate(), lowest);
+                break;
+
+                case UVLevel:
+                lowest = lowestHelper(list.get(i).getUVLevel(), lowest);
+                break;
+
+                case Solarrad:
+                lowest = lowestHelper(list.get(i).getSolarRad(), lowest);
+                break;
+
+                case Barometer:
+                lowest = lowestHelper(list.get(i).getBarometer(), lowest);
+                break;
+            }
+
+        return lowest;
     } 
     
     
+    /**
+     * Calculates the mode.
+     */
+    public double modusHelper(int count, int maxCount,double maxValue, double i)
+    {
+        if(count > maxCount)
+                {
+                    maxCount = count;
+                    maxValue = i;
+                }
+                return maxValue;
+    }
+    public double getModus(ArrayList<RawMeasurement> list, Unit kindOf)
+    {
+        double modus;
+        int count = 0;
+        int maxCount = 0;
+        double maxValue = 0;
+        for(int i = 0; i < list.size();i++)
+            switch (kindOf)
+            {
+                case InsideTemp:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getInsideTemp()==list.get(j).getInsideTemp())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getInsideTemp());
+                break;
+
+                case OutsideTemp:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getOutsideTemp()==list.get(j).getOutsideTemp())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getOutsideTemp());
+                break;
+
+                case Windspeed:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getWindSpeed()==list.get(j).getWindSpeed())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getWindSpeed());
+
+                case OutsideHum:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getOutsideHum()==list.get(j).getOutsideHum())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getOutsideHum());
+                break;
+
+                case RainRate:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getRainRate()==list.get(j).getRainRate())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getRainRate());
+                break;
+
+                case UVLevel:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getUVLevel()==list.get(j).getUVLevel())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getUVLevel());
+                break;
+
+                case Solarrad:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getSolarRad()==list.get(j).getSolarRad())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getSolarRad());
+                break;
+
+                case Barometer:
+                for(int j = 0; j <list.size();j++)
+                {
+                    if(list.get(i).getBarometer()==list.get(j).getBarometer())
+                    {
+                        count = count + 1;
+                    }
+                }
+                maxValue = modusHelper(count, maxCount, maxValue, list.get(i).getBarometer());
+                break;
+            }
+
+        return maxValue;
+    }     
 }
