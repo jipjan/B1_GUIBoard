0566985f1eb624e24d4f5567c5f6df6ea3c32cb1
Robin Hobbel <rm.hobbel@telfort.nl>
2016-10-04 11:40:28 +0200

    Wheatherstation connector added

---
+++ GUIBoard/Measurement.java
@@ -0,0 +1,187 @@
+/**
+ * Class with functions that return the current calculated data.
+ */
+public class Measurement
+{
+   private MetingenHandler M;
+   private RawMeasurement rawMeasurement;
+   
+   public Measurement(RawMeasurement raw)
+   {
+       M = new MetingenHandler();
+       rawMeasurement = raw;
+   }
+   
+   public java.sql.Timestamp Time()
+   {
+       return rawMeasurement.getDateStamp();
+   }
+   public double BarometerData()
+   {
+       return M.luchtdruk(rawMeasurement.getBarometer());
+   }
+  
+   public double TemperatuurInsideData()
+   {
+       return M.temperatuur(rawMeasurement.getInsideTemp());
+   }
+   
+   public double LuchtvochtigheidInsideData()
+   {
+       return rawMeasurement.getInsideHum();
+   }
+   
+   public double TemperatuurOutsideData()
+   {
+       return M.temperatuur(rawMeasurement.getOutsideTemp());
+   }
+   
+   public double WindSpeedData()
+   {
+       return M.windSnelheid(rawMeasurement.getWindSpeed());
+   }
+   
+   public double WindSpeedAvData()
+   {
+       return M.windSnelheid(rawMeasurement.getAvgWindSpeed());       
+   }
+   
+   public String WindRichtingData()
+   {
+       return M.windRichting(rawMeasurement.getWindDir());
+   }
+   
+   public double LuchtvochtigheidOutsideData()
+   {
+       return rawMeasurement.getOutsideHum();
+   }
+   
+   public double RainRateData()
+   {
+       return M.regenmeter(rawMeasurement.getRainRate());
+   }
+   
+   public double UVIndexData()
+   {
+       return M.uvIndex(rawMeasurement.getUVLevel());
+   }
+   
+   public short SolarRad()
+   {
+       return rawMeasurement.getSolarRad();
+   }
+   
+   public double BatteryData()
+   {
+       return M.batterySpanning(rawMeasurement.getBattLevel());
+   }
+   
+   public String SunRiseData()
+   {
+       return M.sunRise(rawMeasurement.getSunrise());
+   }
+   
+   public String SunSetData()
+   {
+       return M.sunSet(rawMeasurement.getSunset());
+   }
+   
+   public String heatIndex()
+    {
+        final double C1 = -42.379,
+                     C2 = 2.04901523,
+                     C3 = 10.14333127,
+                     C4 = -0.22475541,
+                     C5 = -0.00683783,
+                     C6 = -5.481717E-2,
+                     C7 = 1.22874E-3,
+                     C8 = 8.5282E-4,
+                     C9 = -1.99E-6;
+        double T = rawMeasurement.getOutsideTemp()/10;
+        double R = LuchtvochtigheidOutsideData();
+        double T2 = T * T;
+        double R2 = R * R;
+        String answer;
+        double HI;
+        if(TemperatuurOutsideData()>26)
+        {
+            HI = Math.round(((C1 + (C2 * T) + (C3 * R) + (C4 * T * R) + (C5 * T2) + (C6 * R2) + (C7 * T2 * R) + (C8 * T * R2) + (C9 * T2 * R2))-32)/1.8);
+            answer = String.valueOf(HI+"°C");
+        }
+        else
+        {
+            answer = "De temperatuur is te laag om de heat index te berekenen.";
+        }
+        return answer;
+    }
+   
+   public String windChill()
+   {
+       double T = TemperatuurOutsideData();
+       double V = WindSpeedData();
+       String answer;
+       double WC;
+       if(T<10 && V>1.3)
+       {
+           WC = 13.12 + (0.6215*T) - (11.37*(Math.pow(V,0.16))+(0.3965*(T*Math.pow(V,0.16))));
+           answer = String.valueOf(WC+"°C");
+       }
+       else if(T>=10 && V<=1.3)
+       {
+           answer = "De temperatuur is te hoog en de windsnelheid is te laag.";
+       }
+       else if(T>=10) 
+       {
+           answer = "De Temperatuur is te hoog.";
+       }
+       else
+       {
+           answer = "De windsnelheid is te laag.";
+       }
+       return answer; 
+   }
+   
+   public String dewPoint()
+   {
+       double T = TemperatuurOutsideData();
+       double R = LuchtvochtigheidOutsideData();
+       String answer;
+       double D;
+       double a = 17.27;
+       double b = 237.7;
+       double TRL = ((a*T)/(b+T))+ Math.log(R/100);
+       if(0<T && T<100 && 1<R && R<100)
+       {
+           D = Math.round((b*TRL)/(a-TRL));
+           answer = String.valueOf(D+"°C");
+       }
+       else
+       {
+           answer = "Het dauwpunt kan niet worden berekend.";
+       }
+       return answer;
+   }
+   
+   public void PrintAlles()
+   {
+       System.out.println(
+       "Datum en tijd: \t\t\t"+Time()+
+       "\nBarometer: \t\t\t"+BarometerData()+"kPa"+
+       "\nBinnentemperatuur: \t\t"+TemperatuurInsideData()+"°C"+
+       "\nBuitentemperatuur: \t\t"+TemperatuurOutsideData()+"°C"+
+       "\nLuchtvochtigheid binnen: \t"+LuchtvochtigheidInsideData()+"%"+
+       "\nLuchtvochtigheid buiten: \t"+LuchtvochtigheidOutsideData()+"%"+
+       "\nWindsnelheid: \t\t\t"+WindSpeedData()+" Km/U"+
+       "\nWindrichting: \t\t\t"+WindRichtingData()+
+       "\nWindchill: \t\t\t"+windChill()+
+       "\nBatteryspanning: \t\t"+BatteryData()+ " Volt"+
+       "\nUV index: \t\t\t"+ UVIndexData()+
+       "\nZonneStraling: \t\t\t"+SolarRad()+" W/m²"+
+       "\nHeat index: \t\t\t"+heatIndex()+
+       "\nDauwpunt: \t\t\t"+dewPoint()+
+       "\nZonsopgang: \t\t\t"+ SunRiseData()+
+       "\nZonsondergang: \t\t\t"+ SunSetData()
+       
+       );
+   }
+}
---
+++ GUIBoard/MetingenHandler.java
@@ -0,0 +1,122 @@
+/**
+ * This class contains all methods needed to convert the raw mesurements to dutch units
+ * @author Groep B1
+ * @version 1.2
+ */
+public class MetingenHandler
+{
+	/**
+	* Luchtdruk
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De luchtdruk in mbar
+	*/
+	public static double luchtdruk(short mval)
+	{
+		return Math.round( (mval/100d) * 3.3864d);
+	}
+
+	/**
+	* Temperatuur
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De temperatuur in graden Celcius
+	*/
+	public static double temperatuur(short mval)
+	{
+		return Math.round( ((mval / 10d) - 32) /1.8d );
+	}
+
+	/**
+	* Windsnelheid
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De windsnelheid in km/h
+	*/
+	public static double windSnelheid(short mval)
+	{
+		return Math.round( (mval * 1.609344d));
+	}
+
+	/**
+	* Windrichting
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De windrichting In windroos richtingen
+	*/
+	public static String windRichting(short mval)
+	{
+		String mogelijkWindrichting[] = {"N", "NO", "O", "ZO", "Z", "ZW", "W", "NW", "N"};
+		return mogelijkWindrichting[mval /45];
+	}
+
+	/**
+	* Regenmeter
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De hoeveelheid regen in mm
+	*/
+	public static double regenmeter(short mval)
+	{
+		return Math.round((mval *0.2));
+	}
+
+	/**
+	* uvIndex
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De windrichting in graden
+	*/
+	public static double uvIndex(short mval)
+	{
+		return Math.round((mval /10d));
+	}
+
+	/**
+	* batterySpanning
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De battery spanning in Volt
+	*/
+	public static double batterySpanning(short mval)
+	{
+		return Math.round(((mval *300) /512d) /100d);
+	}
+
+	/**
+	 * TimeShortToString
+	 *
+	 * @param shortTime 	de tijd gegeven in een short
+	 * @return de tijd gegeven in een string in hh:mm notatie
+	 */
+	private static String TimeShortToString(short shortTime)
+	{
+		String toReturn = Short.toString(shortTime);
+		if (toReturn.length() == 3)
+			return "0" + toReturn.substring(0,1) + ":" + toReturn.substring(1, 3);
+		else
+			return toReturn.substring(0,2) + ":" + toReturn.substring(2, 4);
+	}
+
+	/**
+	* sunRise
+	*
+	* @param mval	Meetwaarde van het vp2pro weerstation
+	* @return Zonsopkomst in hh:mm notatie
+	*/
+	public static String sunRise(short mval)
+	{
+		return TimeShortToString(mval);
+	}
+
+	/**
+	* sunSet
+	*
+	* @param mval	Meetwaarde van het vp2pro weerstation
+	* @return Zonsondergang in hh:mm notatie
+	*/
+	public static String sunSet(short mval)
+	{
+	   return TimeShortToString(mval);
+	}
+}
