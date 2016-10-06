a0bf9b9d208507bf4a8fdcffec394f804ddc33cc
Jaap-Jan de Wit <jipjan@gmail.com>
2016-10-06 16:54:00 +0200

    javadocs toegevoegd en statistics refactored

--- GUIBoard/Statistics.java
+++ GUIBoard/Statistics.java
@@ -4,149 +4,91 @@ import java.util.*;
  */
 public class Statistics
 {
+    // private list to use in the statistics calculations.
     private ArrayList<RawMeasurement> list;
+    
+    /**
+     * Enum with all the possible units to use in the statistics calculations.
+     */
+    public enum Unit { InsideTemp, OutsideTemp, Windspeed, OutsideHum, RainRate, UVLevel, Solarrad, Barometer };
 
+    /**
+     * Constructor which takes the measurements you want to analyze.
+     * @param measurements  The list with measurements to run the methods on.
+     */
     public Statistics(ArrayList<RawMeasurement> measurements)
     {
         list = measurements;
+    }   
+
+    /**
+     * Get specific unit from the list.
+     * @param i     Index to use for the list
+     * @param unit  Unit to retrieve from the list
+     * @return      The value from the list
+     */
+    private short getValue(int i, Unit unit)
+    {
+        switch (unit)
+        {
+            case InsideTemp: return list.get(i).getInsideTemp();
+            case OutsideTemp: return list.get(i).getOutsideTemp();
+            case Windspeed: return list.get(i).getWindSpeed();
+            case OutsideHum: return list.get(i).getOutsideHum();
+            case RainRate: return list.get(i).getRainRate();
+            case UVLevel: return list.get(i).getUVLevel();
+            case Solarrad: return list.get(i).getSolarRad();
+            case Barometer: return list.get(i).getBarometer();
+        }
+        return 0;
     }
 
-    public enum Unit { InsideTemp, OutsideTemp, Windspeed, OutsideHum, RainRate, UVLevel, Solarrad, Barometer };
-
-    public double getAverage(Unit kindOf)
+    /**
+     * Get average of the value in the measurements.
+     * @param unit  Unit to retrieve from the list
+     * @return      The average
+     */
+    public int getAverage(Unit unit)
     {
-        double average = 0;
-        for(int i = 0; i < list.size();i++)
-            switch (kindOf)
-            {
-                case InsideTemp:
-                average += list.get(i).getInsideTemp();
-                break;
-
-                case OutsideTemp:
-                average += list.get(i).getOutsideTemp();
-                break;
-
-                case Windspeed:
-                average += list.get(i).getWindSpeed();
-                break;
-
-                case OutsideHum:
-                average += list.get(i).getOutsideHum();
-                break;
-
-                case RainRate:
-                average += list.get(i).getRainRate();
-                break;
-
-                case UVLevel:
-                average += list.get(i).getUVLevel();
-                break;
-
-                case Solarrad:
-                average += list.get(i).getSolarRad();
-                break;
-
-                case Barometer:
-                average += list.get(i).getBarometer();
-                break;
-            }
-
-        return average /list.size();
+        int average = 0;
+        for(int i = 0; i < list.size(); i++)
+            average += getValue(i, unit);
+        return average / list.size();
     } 
 
     /**
-     * Assists getLowest.
+     * Get lowest value of the unit in the list.
+     * @param unit  The unit to retrieve the lowest value from.
+     * @return      Lowest value.
      */
-    public double lowestHelper(double L, double lowest)
+    public short getLowest(Unit unit)
     {
-        if(lowest > L)
+        short lowest = Short.MAX_VALUE;
+        for(int i = 0; i < list.size(); i++)
         {
-            lowest = L;
+            short value = getValue(i, unit);
+            if (lowest > value)
+                lowest = value;
         }
         return lowest;
     }
 
-    public double getLowest(ArrayList<RawMeasurement> list, Unit kindOf)
-    {
-        double lowest = Integer.MAX_VALUE;
-        for(int i = 0; i < list.size();i++)
-            switch (kindOf)
-            {
-                case InsideTemp:
-                lowest = lowestHelper(list.get(i).getInsideTemp(), lowest);
-                break;
-
-                case OutsideTemp:
-                lowest = lowestHelper(list.get(i).getOutsideTemp(), lowest);
-                break;
-
-                case Windspeed:
-                lowest = lowestHelper(list.get(i).getWindSpeed(), lowest);
-                break;
-
-                case OutsideHum:
-                lowest = lowestHelper(list.get(i).getOutsideHum(), lowest);
-                break;
-
-                case RainRate:
-                lowest = lowestHelper(list.get(i).getRainRate(), lowest);
-                break;
-
-                case UVLevel:
-                lowest = lowestHelper(list.get(i).getUVLevel(), lowest);
-                break;
-
-                case Solarrad:
-                lowest = lowestHelper(list.get(i).getSolarRad(), lowest);
-                break;
-
-                case Barometer:
-                lowest = lowestHelper(list.get(i).getBarometer(), lowest);
-                break;
-            }
-
-        return lowest;
-    } 
-
+    /**
+     * Get modus of the units in the list.
+     * @param unit  The unit to retrieve the modus of.
+     * @return      The modus.
+     */
     public short getModus(Unit unit)
     {
         HashMap<Short, Integer> map = new HashMap<Short, Integer>();
         for (int i = 0; i < list.size(); i++)
-            switch (unit)
-            {
-                case InsideTemp:
-                addItemToMap(map, list.get(i).getInsideTemp());
-                break;
-
-                case OutsideTemp:
-                addItemToMap(map, list.get(i).getOutsideTemp());
-                break;
-
-                case Windspeed:
-                addItemToMap(map, list.get(i).getWindSpeed());
-                break;
-
-                case OutsideHum:
-                addItemToMap(map, list.get(i).getOutsideHum());
-                break;
-
-                case RainRate:
-                addItemToMap(map, list.get(i).getRainRate());
-                break;
-
-                case UVLevel:
-                addItemToMap(map, list.get(i).getUVLevel());
-                break;
-
-                case Solarrad:
-                addItemToMap(map, list.get(i).getSolarRad());
-                break;
-
-                case Barometer:
-                addItemToMap(map, list.get(i).getBarometer());
-                break;
-            }
+        {
+            short item = getValue(i, unit);
+            if (map.containsKey(item))
+                map.put(item, map.get(item) + 1);
+            else
+                map.put(item, 1);
+        }
         short median = 0;
         int amount = 0;
         Iterator it = map.entrySet().iterator();
@@ -158,202 +100,61 @@ public class Statistics
         }
         return median;
     }
-    
-    private void addItemToMap(HashMap<Short, Integer> map, short item)
-    {
-        if (map.containsKey(item))
-            map.put(item, map.get(item)+1);
-        else
-            map.put(item, 1);
-    }
 
     /**
-     * Assists getHighest.
+     * Get highest value of the unit in the list.
+     * @param unit  The unit to retrieve the highest value from.
+     * @return      Highest value.
      */
-    public double highestHelper(double H, double highest)
+    public short getHighest(Unit unit)
     {
-        if(highest < H)
+        short highest = Short.MIN_VALUE;
+        for(int i = 0; i < list.size(); i++)
         {
-            highest = H;
+            short value = getValue(i, unit);
+            if (highest < value)
+                highest = value;
         }
         return highest;
     }
 
-    public double getHighest(Unit kindOf)
-    {
-        double highest = Integer.MIN_VALUE;
-        for(int i = 0; i < list.size();i++)
-            switch (kindOf)
-            {
-                case InsideTemp:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);
-                break;
-
-                case OutsideTemp:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);
-                break;
-
-                case Windspeed:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);
-                break;
-
-                case OutsideHum:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);
-                break;
-
-                case RainRate:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);;
-                break;
-
-                case UVLevel:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);
-                break;
-
-                case Solarrad:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);
-                break;
-
-                case Barometer:
-                highest = highestHelper(list.get(i).getInsideTemp(), highest);
-                break;
-            }
-        return highest;
-    } 
-
+    /**
+     * Calculate deviant from a unit in the list.
+     * @param unit  The unit to calculate the deviant of
+     * @return      The deviant as a double
+     */
     public double getDeviant(Unit unit)
     {
         double deviant = 0;
         double average = getAverage(unit);
-        switch (unit)
-        {
-            case InsideTemp:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getInsideTemp() - average, 2);
-            break;
-
-            case OutsideTemp:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getOutsideTemp() - average, 2);
-            break;
-
-            case Windspeed:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getWindSpeed() - average, 2);
-            break;
-
-            case OutsideHum:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getOutsideHum() - average, 2);
-            break;
-
-            case RainRate:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getRainRate() - average, 2);
-            break;
-
-            case UVLevel:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getUVLevel() - average, 2);
-            break;
-
-            case Solarrad:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getSolarRad() - average, 2);
-            break;
-
-            case Barometer:
-            for (int i = 0; i < list.size(); i++)
-                deviant += Math.pow(list.get(i).getBarometer() - average, 2);
-            break;
-        }
+        for (int i = 0; i < list.size(); i++)
+            deviant += Math.pow(getValue(i, unit) - average, 2);
         return Math.pow(deviant / (list.size() - 1), 0.5);
     }
 
-    public short getMedian(Unit unit)
+    /**
+     * Calculate Median from a unit in the list.
+     * @param unit  The unit to calculate the median of
+     * @return      The median
+     */
+    public double getMedian(Unit unit)
     {
-        double median = 0;
-        ArrayList<Short> medianList = new ArrayList<Short>();
-        for(int i = 0; i < list.size();i++)
-            switch (unit)
-            {
-                case InsideTemp:
-                medianList.add(list.get(i).getInsideTemp());
-                break;
-
-                case OutsideTemp:
-                medianList.add(list.get(i).getOutsideTemp());
-                break;
-
-                case Windspeed:
-                medianList.add(list.get(i).getWindSpeed());
-                break;
-
-                case OutsideHum:
-                medianList.add(list.get(i).getOutsideHum());
-                break;
-
-                case RainRate:
-                medianList.add(list.get(i).getRainRate());
-                break;
-
-                case UVLevel:
-                medianList.add(list.get(i).getUVLevel());
-                break;
-
-                case Solarrad:
-                medianList.add(list.get(i).getSolarRad());
-                break;
-
-                case Barometer:
-                medianList.add(list.get(i).getBarometer());
-                break;
-            }
-        Collections.sort(medianList);
+        ArrayList<Short> medianList = sortList(unit);
         int middle = medianList.size() / 2;
-        if(medianList.size() % 2 == 0)
-            return (short) Math.round(((medianList.get(middle - 1) + medianList.get(middle)) / 2d));
-        else
-            return medianList.get(middle);
+        return medianList.size() % 2 == 0 ?
+            ((medianList.get(middle - 1) + medianList.get(middle)) / 2d) : medianList.get(middle);
     }
 
+    /**
+     * Build a sorted list based on a unit.
+     * @param unit  The unit to use as sort operator.
+     * @return      The sorted array.
+     */
     private ArrayList<Short> sortList(Unit unit)
     {
         ArrayList<Short> sList = new ArrayList<Short>();
-        for(int i = 0; i < list.size();i++)
-            switch (unit)
-            {
-                case InsideTemp:
-                sList.add(list.get(i).getInsideTemp());
-                break;
-
-                case OutsideTemp:
-                sList.add(list.get(i).getOutsideTemp());
-                break;
-
-                case Windspeed:
-                sList.add(list.get(i).getWindSpeed());
-                break;
-
-                case OutsideHum:
-                sList.add(list.get(i).getOutsideHum());
-                break;
-
-                case RainRate:
-                sList.add(list.get(i).getRainRate());
-                break;
-
-                case UVLevel:
-                sList.add(list.get(i).getUVLevel());
-                break;
-
-                case Solarrad:
-                sList.add(list.get(i).getSolarRad());
-                break;
-
-                case Barometer:
-                sList.add(list.get(i).getBarometer());
-                break;
-            }
+        for(int i = 0; i < list.size(); i++)
+            sList.add(getValue(i, unit));
         Collections.sort(sList);
         return sList;
     }
