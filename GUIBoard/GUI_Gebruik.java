485b4a207829c9471134eef9347bd7d4f3d24deb
Obama <maartenmeyer8@gmail.com>
2016-10-06 09:27:53 +0200

    fix median and added test

--- GUIBoard/GUI_Gebruik.java
+++ GUIBoard/GUI_Gebruik.java
@@ -31,4 +31,28 @@ public class GUI_Gebruik
         System.out.println(stat.getAverage(Statistics.Unit.OutsideTemp));
         System.out.println(stat.getDeviant(Statistics.Unit.OutsideTemp));
     }
+    
+    public void testMedian()
+    {
+        ArrayList<RawMeasurement> list = new ArrayList<RawMeasurement>();
+        
+        RawMeasurement item = new RawMeasurement();
+        item.setOutsideTemp((short) 5);
+        
+        RawMeasurement item2 = new RawMeasurement();
+        item2.setOutsideTemp((short)-3);
+        
+        RawMeasurement item3 = new RawMeasurement();
+        item3.setOutsideTemp((short)10);
+        
+        list.add(item);
+        list.add(item2);
+        list.add(item3);
+        list.add(item3);
+        
+        
+        Statistics stat = new Statistics(list);
+        
+        System.out.println(stat.getMedian(Statistics.Unit.OutsideTemp));
+    }
 }
--- GUIBoard/Statistics.java
+++ GUIBoard/Statistics.java
@@ -7,55 +7,55 @@ import java.util.Collections;
 public class Statistics
 {
     private ArrayList<RawMeasurement> list;
-    
+
     public Statistics(ArrayList<RawMeasurement> measurements)
     {
         list = measurements;
     }
-    
+
     public enum Unit { InsideTemp, OutsideTemp, Windspeed, OutsideHum, RainRate, UVLevel, Solarrad, Barometer };
 
     public double getAverage(Unit kindOf)
     {
         double average = 0;
         for(int i = 0; i < list.size();i++)
-        switch (kindOf)
-        {
-            case InsideTemp:
-            average += list.get(i).getInsideTemp();
-            break;
+            switch (kindOf)
+            {
+                case InsideTemp:
+                average += list.get(i).getInsideTemp();
+                break;
 
-            case OutsideTemp:
-            average += list.get(i).getOutsideTemp();
-            break;
+                case OutsideTemp:
+                average += list.get(i).getOutsideTemp();
+                break;
 
-            case Windspeed:
-            average += list.get(i).getWindSpeed();
-            break;
-            
-            case OutsideHum:
-            average += list.get(i).getOutsideHum();
-            break;
-            
-            case RainRate:
-            average += list.get(i).getRainRate();
-            break;
-            
-            case UVLevel:
+                case Windspeed:
+                average += list.get(i).getWindSpeed();
+                break;
+
+                case OutsideHum:
+                average += list.get(i).getOutsideHum();
+                break;
+
+                case RainRate:
+                average += list.get(i).getRainRate();
+                break;
+
+                case UVLevel:
                 average += list.get(i).getUVLevel();
-            break;
-            
-            case Solarrad:
+                break;
+
+                case Solarrad:
                 average += list.get(i).getSolarRad();
-            break;
-            
-            case Barometer:
+                break;
+
+                case Barometer:
                 average += list.get(i).getBarometer();
-            break;
-        }
+                break;
+            }
         return average /list.size();
     } 
-    
+
     public double getDeviant(Unit unit)
     {
         double deviant = 0;
@@ -76,27 +76,27 @@ public class Statistics
             for (int i = 0; i < list.size(); i++)
                 deviant += Math.pow(list.get(i).getWindSpeed() - average, 2);
             break;
-            
+
             case OutsideHum:
             for (int i = 0; i < list.size(); i++)
                 deviant += Math.pow(list.get(i).getOutsideHum() - average, 2);
             break;
-            
+
             case RainRate:
             for (int i = 0; i < list.size(); i++)
                 deviant += Math.pow(list.get(i).getRainRate() - average, 2);
             break;
-            
+
             case UVLevel:
             for (int i = 0; i < list.size(); i++)
                 deviant += Math.pow(list.get(i).getUVLevel() - average, 2);
             break;
-            
+
             case Solarrad:
             for (int i = 0; i < list.size(); i++)
                 deviant += Math.pow(list.get(i).getSolarRad() - average, 2);
             break;
-            
+
             case Barometer:
             for (int i = 0; i < list.size(); i++)
                 deviant += Math.pow(list.get(i).getBarometer() - average, 2);
@@ -104,57 +104,51 @@ public class Statistics
         }
         return Math.pow(deviant / (list.size() - 1), 0.5);
     }
-    
-        public double getMedian(Unit unit)
+
+    public short getMedian(Unit unit)
     {
         double median = 0;
-        ArrayList medianList = new ArrayList();
+        ArrayList<Short> medianList = new ArrayList<Short>();
         for(int i = 0; i < list.size();i++)
-        switch (unit)
-        {
-            case InsideTemp:
-            medianList += list.get(i).getInsideTemp();
-            break;
-            
-            case OutsideTemp:
-            medianList += list.get(i).getOutsideTemp();
-            break;
+            switch (unit)
+            {
+                case InsideTemp:
+                medianList.add(list.get(i).getInsideTemp());
+                break;
 
-            case Windspeed:
-            medianList += list.get(i).getWindSpeed();
-            break;
-            
-            case OutsideHum:
-            medianList += list.get(i).getOutsideHum();
-            break;
-            
-            case RainRate:
-            medianList += list.get(i).getRainRate();
-            break;
-            
-            case UVLevel:
-            medianList += list.get(i).getUVLevel();
-            break;
-            
-            case Solarrad:
-            medianList += list.get(i).getSolarRad();
-            break;
-            
-            case Barometer:
-            medianList += list.get(i).getBarometer();
-            break;            
-        }
+                case OutsideTemp:
+                medianList.add(list.get(i).getOutsideTemp());
+                break;
+
+                case Windspeed:
+                medianList.add(list.get(i).getWindSpeed());
+                break;
+
+                case OutsideHum:
+                medianList.add(list.get(i).getOutsideHum());
+                break;
+
+                case RainRate:
+                medianList.add(list.get(i).getRainRate());
+                break;
+
+                case UVLevel:
+                medianList.add(list.get(i).getUVLevel());
+                break;
+
+                case Solarrad:
+                medianList.add(list.get(i).getSolarRad());
+                break;
+
+                case Barometer:
+                medianList.add(list.get(i).getBarometer());
+                break;
+            }
         Collections.sort(medianList);
-        
-        int middle = ((medianList.size) / 2);
-        if(medianList.size % 2 == 0){
-           int numberA = medianList[middle];
-           int numberB = medianList[middle-1];
-           median = (numberA + numberB) / 2;
-        }
-        else{
-           median = medianList[middle + 1];
-        }
-        return median /list.size();
-    } 
+        int middle = medianList.size() / 2;
+        if(medianList.size() % 2 == 0)
+            return (short) Math.round(((medianList.get(middle - 1) + medianList.get(middle)) / 2d));
+        else
+            return medianList.get(middle);
+    }
 }
