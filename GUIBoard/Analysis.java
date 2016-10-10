dfd6421fee9de63339cab6b486bb030433b3f1f3
Jaap-Jan de Wit <jipjan@gmail.com>
2016-10-10 14:11:42 +0200

    make class non-static, load all data on construction to use for analysis. Add Javadocs

--- GUIBoard/Analysis.java
+++ GUIBoard/Analysis.java
@@ -1,22 +1,37 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.sql.Timestamp;
+
 /**
- * Write a description of class Analysis here.
+ * This class contains all analysis methods for data from the weatherstation.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Group B1
  */
 public class Analysis
-{
-    public static boolean hasHeatWave(Period periode)
+{  
+    Period _period;
+    ArrayList<RawMeasurement> _list;
+    
+    public Analysis(Period measurements)
+    {
+        _period = measurements;
+        _list = measurements.getRawMeasurements(new WeatherStation());
+    }
+    
+    
+    /*
+     * Check if a certain period has a heatwave
+     * @return  Returns if the period contains a heatwave.
+     */
+    public boolean hasHeatWave()
     {        
-        // temp 25 == 770
-        // temp 30 == 860
-        ArrayList<RawMeasurement> list = periode.getRawMeasurements(new WeatherStation());
+        // temp 25 == 770 - fahrenheit * 10
+        // temp 30 == 860 - fahrenheit * 10
         int amount25 = 0;
         int amount30 = 0;        
-        for (int i = 0; i < list.size(); i++)
+        for (int i = 0; i < _list.size(); i++)
         {
-            int temp = list.get(i).getOutsideTemp();
+            int temp = _list.get(i).getOutsideTemp();
             if (temp >= 770)
             {
                 if (temp >= 860)
@@ -34,14 +49,17 @@ public class Analysis
         return false;
     }
 
-    public static int maxAmountOfRainSeqDays(Period periode)
+    /*
+     * Check the most amount of rain that has fallen in sequence.
+     * @return  The amount of rain that has fallen.
+     */
+    public int maxAmountOfRainSeqDays()
     {
-        ArrayList<RawMeasurement> list = periode.getRawMeasurements(new WeatherStation());
         int maxAmount = 0;
         int amount = 0;
-        for (int i = 0; i < list.size(); i++)
+        for (int i = 0; i < _list.size(); i++)
         {
-            int rain = list.get(i).getRainRate();
+            int rain = _list.get(i).getRainRate();
             if (rain > 0)
                 amount += rain;
             else
@@ -54,24 +72,27 @@ public class Analysis
         return maxAmount;
     }
 
-    public static Period getLongestRainfall(Period period)
+    /*
+     * Check the longest period of rainfall.
+     * @return  A period object with start and stopdate with longest period of rain
+     */
+    public Period getLongestRainfall()
     {
         int counter = 0;
         int longestPeriod = 0;
         Period newPeriod = new Period();
-        ArrayList<RawMeasurement> PeriodRawMeasurements = period.getRawMeasurements(new WeatherStation());
-
-        for(int i = 0; i < PeriodRawMeasurements.size(); i++)
+        
+        for(int i = 0; i < _list.size(); i++)
         {   
-            if(PeriodRawMeasurements.get(i).getRainRate() > 0)
+            if(_list.get(i).getRainRate() > 0)
                 counter++;
             else
             {   
                 if(counter > longestPeriod)
                 {
                     longestPeriod = counter;
-                    newPeriod.setStart(PeriodRawMeasurements.get(i - counter).getDateStamp().toLocalDateTime().toLocalDate());
-                    newPeriod.setEnd(PeriodRawMeasurements.get(i).getDateStamp().toLocalDateTime().toLocalDate());
+                    newPeriod.setStart(_list.get(i - counter).getDateStamp().toLocalDateTime().toLocalDate());
+                    newPeriod.setEnd(_list.get(i).getDateStamp().toLocalDateTime().toLocalDate());
                 }                
                 counter = 0;
             }
@@ -79,23 +100,27 @@ public class Analysis
         return newPeriod;
     }
 
-    public static Period maxDroogePeriode(Period periode, int mNeerslag)//voer de periode in
+    /*
+     * Check the longest period of drought.
+     * @param mNeerslag Upper bound of what the limit is for drought
+     * @return  Period of longest drought
+     */
+    public Period longestDrought(int mNeerslag)//voer de periode in
     {
-        ArrayList<RawMeasurement> list = periode.getRawMeasurements(new WeatherStation());// voer de periode array in in list
-        ArrayList<java.sql.Timestamp> time = new ArrayList<java.sql.Timestamp>();
+        ArrayList<Timestamp> time = new ArrayList<Timestamp>();
         //initaliseerst belangrijke telwaarden
         int count = 0;
         int maxCount = 0;
         //geeft een waarde vor de maximale neerslag die mag vallen o nog mee te tellen.
         int maximaleNeerslag = mNeerslag;
         //Dit i voor de weergave van de datums
-        java.sql.Timestamp Begindate = list.get(0).getDateStamp();
-        java.sql.Timestamp Einddate = list.get(0).getDateStamp();
-        java.sql.Timestamp TijdelijkeDate = list.get(0).getDateStamp();
+        Timestamp Begindate = _list.get(0).getDateStamp();
+        Timestamp Einddate = _list.get(0).getDateStamp();
+        Timestamp TijdelijkeDate = _list.get(0).getDateStamp();
         //
-        for(int i = 0; i < list.size();i++)
+        for(int i = 0; i < _list.size();i++)
         {
-            if(list.get(i).getRainRate()== maximaleNeerslag)
+            if(_list.get(i).getRainRate()== maximaleNeerslag)
             {
                 count += 1;
             }
@@ -106,32 +131,36 @@ public class Analysis
                     maxCount = count;
                     //
                     Begindate = TijdelijkeDate;
-                    Einddate = list.get(i).getDateStamp();
+                    Einddate = _list.get(i).getDateStamp();
                     //
                     count = 0;
                 }
-                TijdelijkeDate = list.get(i).getDateStamp();
+                TijdelijkeDate = _list.get(i).getDateStamp();
             }
         }
         return new Period(Begindate.toLocalDateTime().toLocalDate(), Einddate.toLocalDateTime().toLocalDate());
     }
     
-    public static Period Get_Longest_temperature_rise(Period periodToCheckOut, WeatherStation weatherStation)
+    /*
+     * Check what the longest period of temperature rise is
+     * @return          Period with longest temperature rise.
+     */
+    public Period getLongestTemperatureRise()
     {
-        LocalDate beginDate = periodToCheckOut.getStart();
+        LocalDate beginDate = _period.getStart();
         
         Period longestTempRisePeriod = new Period();
         longestTempRisePeriod.setStart(beginDate);
         longestTempRisePeriod.setEnd(beginDate.minusDays(1));
         
-        double lastTemp = getAvgTempOfDay(beginDate, weatherStation);
+        double lastTemp = getAvgTempOfDay(beginDate);
         double tempOfI= 0;
         
         int periodLength = 0;
-        for (int i = 1; i < periodToCheckOut.numberOfDays(); i++)
+        for (int i = 1; i < _period.numberOfDays(); i++)
         {
             LocalDate dateOfI = beginDate.plusDays(i);
-            tempOfI = getAvgTempOfDay(dateOfI, weatherStation);
+            tempOfI = getAvgTempOfDay(dateOfI);
             periodLength++;
             
             if (tempOfI <= lastTemp)
@@ -149,15 +178,15 @@ public class Analysis
         
         if (longestTempRisePeriod.getStart().compareTo(longestTempRisePeriod.getEnd()) <= 0)
         {
-            longestTempRisePeriod.setEnd(periodToCheckOut.getEnd());
+            longestTempRisePeriod.setEnd(_period.getEnd());
         }
         return longestTempRisePeriod;
     }
-        
-    private static double getAvgTempOfDay(LocalDate day, WeatherStation weatherStation)
+    
+    private double getAvgTempOfDay(LocalDate day)
     {
         Period dayPeriod = new Period(day, day);        
-        ArrayList<RawMeasurement> dayRawMeasurement = dayPeriod.getRawMeasurements(weatherStation);        
+        ArrayList<RawMeasurement> dayRawMeasurement = dayPeriod.getRawMeasurements(new WeatherStation());        
         Statistics statisticOfDay = new Statistics(dayRawMeasurement);
         return statisticOfDay.getAverage(Statistics.Unit.OutsideTemp);
     }
