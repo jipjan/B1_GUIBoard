7fbeb94d1d5debefee43ebd2e58b06a7b40d0b4f
Robin Hobbel <rm.hobbel@telfort.nl>
2016-10-11 14:49:38 +0200

    Fixed Negative numbers, allowed negative nubers to fit in 3 digit display's

--- GUIBoard/GUI_Gebruik.java
+++ GUIBoard/GUI_Gebruik.java
@@ -2,8 +2,96 @@ import java.util.ArrayList;
 
 public class GUI_Gebruik
 {
+    private WeatherStation ws;
+
     public GUI_Gebruik() 
     {
         IO.init();
+        ws = new WeatherStation();
     }
+
+    public void printMostRecentOutsideTemp()
+    {
+        GUI_Digits_Helper.clearAll(); 
+        GUI_Matrix_Helper.clrDisplay();
+        int vakje = 0x10;//is het bovenste vakje
+        double getal = MetingenHandler.temperatuur(ws.getMostRecentOutsideTemp());
+        System.out.println(getal);
+        if(getal < 0) //als het een negatief getal is
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                int temp;
+                if(getal <= -10)
+                    temp = (int)(getal*10);
+                else
+                    temp = (int)(getal);
+                IO.writeShort(vakje,GUI_Digits_Helper.digitToSegments(temp%10,vakje==0x12)); 
+                vakje = vakje + 0x02;
+                getal = getal / 10;
+            }
+        }
+        else //als het een positief getal is
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                int temp = (int)(getal*10);
+                IO.writeShort(vakje,GUI_Digits_Helper.digitToSegments(temp%10,vakje==0x12)); 
+                vakje = vakje + 0x02;
+                getal = getal / 10;
+            }
+        }
+
+        GUI_Matrix_Helper.stringToMatrix("Buitentemperatuur");
+    }
+
+    public void testNegatief()
+    {
+        GUI_Digits_Helper.clearAll(); 
+        GUI_Matrix_Helper.clrDisplay();
+        int vakje = 0x10;//is het bovenste vakje
+        double getal = -2.5;
+        System.out.println(getal);
+        if(getal < 0) //als het een negatief getal is
+        {
+            getal = Math.abs(getal);
+            int temp;
+            if(getal >= 10)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+
+                    temp = (int)(getal);
+                    IO.writeShort(vakje,GUI_Digits_Helper.digitToSegments(temp%10,false));
+                    vakje = vakje + 0x02;
+                    getal = getal / 10;
+                }
+            }
+            else
+            {
+                for(int j = 0; j < 2; j++)
+                {
+
+                    temp = (int)(getal*10);
+                    IO.writeShort(vakje,GUI_Digits_Helper.digitToSegments(temp%10,vakje==0x12)); 
+                    vakje = vakje + 0x02;
+                    getal = getal / 10;
+                } 
+            }
+            IO.writeShort(vakje,0x140);
+        }
+        else //als het een positief getal is
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                int temp = (int)(getal*10);
+                IO.writeShort(vakje,GUI_Digits_Helper.digitToSegments(temp%10,vakje==0x12)); 
+                vakje = vakje + 0x02;
+                getal = getal / 10;
+            }
+        }
+
+        GUI_Matrix_Helper.stringToMatrix("Buitentemperatuur");
+    }
+
 }
