ffd45bd5367c88d3141f7fa26987578c831755d9
Arthur van Strien <arthurvanstrien@gmail.com>
2016-10-13 12:24:15 +0200

    created methods to change the period of the TestAnalysis class.

--- GUIBoard/TestAnalasis.java
+++ GUIBoard/TestAnalasis.java
@@ -17,6 +17,30 @@ public class TestAnalasis
         analysis = new Analysis(period);
         IO.init();
     }
+    
+    public void setPeriodTo2013()
+    {
+        period.setStart(2013, 1, 1);
+        period.setEnd(2013, 12, 31);
+    }
+    
+    public void setPeriodTo2014()
+    {
+        period.setStart(2014, 1, 1);
+        period.setEnd(2014, 12, 31);
+    }
+    
+    public void setPeriodTo2015()
+    {
+        period.setStart(2015, 1, 1);
+        period.setEnd(2015, 12, 31);
+    }
+    
+    public void setPeriodTo2016()
+    {
+        period.setStart(2016, 1, 1);
+        period.setEnd(2016, 12, 31);
+    }
 
     public void testHasHeatWave()
     {
